da55b5d5d1e34781b3319f3fe4ff3ca8536793bb
Trey Bean <treybean@MacBook.local>
2017-08-19 20:19:28 -0600

    Implemented depth first search

--- search.py
+++ search.py
@@ -67,9 +67,69 @@ def tinyMazeSearch(problem):
   w = Directions.WEST
   return  [s,s,w,s,w,w,s,w]
 
+class Node:
+
+    """A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state.  Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node.  Other functions
+    may add an f and h value; see best_first_graph_search and astar_search for
+    an explanation of how the f and h values are handled. You will not need to
+    subclass this class."""
+
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, successor)
+                for successor in problem.getSuccessors(self.state)]
+
+    def child_node(self, problem, successor):
+        """[Figure 3.10]"""
+        # next = problem.result(self.state, action)
+        return Node(successor[0], self, successor[1], self.path_cost + successor[2])
+
+    def solution(self):
+        """Return the sequence of actions to go from the root to this node."""
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        """Return a list of nodes forming the path from the root to this node."""
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+
+    # We want for a queue of nodes in breadth_first_search or
+    # astar_search to have no duplicated states, so we treat nodes
+    # with the same state as equal. [Problem: this may not be what you
+    # want in other contexts.]
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        return hash(self.state)
+
 def depthFirstSearch(problem):
   """
-  Search the deepest sΩszd fwedxWEXWSffs in the search tree first
+  Search the deepest in the search tree first
   [2nd Edition: p 75, 3rd Edition: p 87]
 
   Your search algorithm needs to return a list of actions that reaches
@@ -84,9 +144,24 @@ def depthFirstSearch(problem):
   print "Start's successors:", problem.getSuccessors(problem.getStartState())
   """
   "*** YOUR CODE HERE ***"
-  print "Start:", problem.getStartState()
-  print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-  print "Start's successors:", problem.getSuccessors(problem.getStartState())
+  frontier = util.Stack()
+  frontier.push(Node(problem.getStartState()))
+  explored = set()
+
+  while frontier:
+    node = frontier.pop()
+
+    if problem.isGoalState(node.state):
+    #   print("FOUND GOAL: {}".format(node))
+      return node.solution()
+
+    explored.add(node.state)
+
+    frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and
+                        child not in frontier)
+
+  return None
 
 def breadthFirstSearch(problem):
   """
